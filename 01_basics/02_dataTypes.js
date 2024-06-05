501122848bd495ee535db500b4be50afb6d243a5
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-06-05 15:24:47 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //this line ensures that all the code written is treated as the newer version of code of JS (A good practice)
+
+//alert("Hello")   // this will give errors as we are using node js and not browser 
+
+console.log(3+3); //In ecah and every case we should ensure that the code readability is high for our code. 
+
+let name = "Anish"  //string dataType
+let age = 18    //integer data type
+let isIt = true  //boolean datatype
+
+//number, bigint, string, boolean, NULL (its a stnad-alone value, instead of a datatype)
+//undefined => no value has been assigned to a variable etc
+//symbol => for uniqueness 
+
+//object
+
+console.log(typeof "Anish");  //will return string (typeof retrun the datatype of a variable)
+console.log(typeof age) //will return number
+
+console.log(typeof null); // returns object (no clear reason to this, maybe a mistake in the language or something)
+console.log(typeof undefined); //this returns undefined, i.e., undefined has a specian data type but null does not has
