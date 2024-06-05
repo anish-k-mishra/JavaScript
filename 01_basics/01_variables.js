b71f2b8c461e0dba93149732115c0b0e4ae05761
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-06-05 15:27:19 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const id = 123               //id can not be modified further as it is initialized with const variable
 let name = "Anish"           //one way to declare a variable
 var email = "anishmishra@google.com"  //another way to declare a variable (used very rarely because of the problem of block scope and functional scope)
-passwd = "123456"            // yet another way to declare a variable in JS (not a good practice)
+passwd = "123456"            // yet again a way to declare a variable in JS (not a good practice)
 let state; //in this case the value of this variable is undefined
 console.log(id);
 console.table([id, name, email, passwd, state]) // prints all the variables mentioned in the square brackets in the form of a table
