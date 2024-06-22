4c7a332c67912e396e18848f56f879db041f6585
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-06-22 14:18:31 +0000

    while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`The value of index is ${index}`);
+    index++;
+}
+
+let score = 0;
+do{
+    console.log(`Score : ${score}`);
+    score++;
+}while(score<=10)
